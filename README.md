3b97a2dd7db053bf1a59b784440504dc4d4e3aff
Ismail ZAHIR <ismailza407@gmail.com>
2023-07-20 14:05:30 +0100

    upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+
+# Smart Care
+Hackathon GEIW 2023 ENSET Mohammedia - BLOCKCHAIN ET IA AU SERVICE DE LA SANTE
+</div>
---
+++ lib/Pages/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:medrec/Utils/routes.dart';
+
+class Splash2 extends StatefulWidget {
+  const Splash2({Key? key}) : super(key: key);
+
+  @override
+  State<Splash2> createState() => _Splash2State();
+}
+
+class _Splash2State extends State<Splash2> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3),
+        () => Navigator.popAndPushNamed(context, MyRoutes.loginPage));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(16),
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(500),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 50,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Image.asset(
+                'assets/Icons/icon.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "S M A R T   C A R E",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Manage Your Medical Records",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
